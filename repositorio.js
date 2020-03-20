3e0d18b5c0e3ef4502fb64f65f430b98e29b540a
oxaciano <iasisalomon@gmail.com>
2020-03-19 21:44:48 -0300

    grandes frases del deporte

---
+++ repositorio.js
@@ -0,0 +1,3 @@
+console.log ("You can't be afraid to fail. It's the only way you succeed – you're not gonna succeed all the time. –LeBron James");
+console.log ("If you do the work, you get rewarded. There are no short cuts in life. – Michael Jordan");
+console.log ("You can't live a perfect day without doing something for someone who will never be able to repay you. – John Wooden");
